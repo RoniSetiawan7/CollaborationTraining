ab06974755e1fe2230d38bbca5dec2ab3a448ad3
titaanela <titanela1509@gmail.com>
2019-10-18 17:25:32 +0700

    modifikasi index -tita

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h2> Selamat berlatih menjadi Collaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Roni Setiawan (175410118)</h3>"
+echo "<h3>Tita Nela F (175410112)</h3>"
 ?>
