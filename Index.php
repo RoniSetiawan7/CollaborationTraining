f4f10c0024cbde7c073c24f1e108f525775b1766
andikabudi <dhikagamor@gmail.com>
2019-10-18 18:08:51 +0700

    modifikasi index

--- Index.php
+++ Index.php
@@ -1,6 +1,5 @@
 <?php
-echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h2> Selamat berlatih menjadi Collaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Andika Budi K.( 175410113 ) </h3>"
-
-?> 
+echo "<h3>Andika BUdi K (175410113)</h3>"
+?>
